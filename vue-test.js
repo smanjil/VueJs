fce6106186e9bb2067798d64feacc6d5c3e6581a
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-11-14 10:59:05 +0545

    Vue js test files

---
+++ vue-test.js
@@ -0,0 +1,23 @@
+
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+
+    var app2 = new Vue({
+        el: '#app-2',
+        data: {
+            message: 'You loaded this page on ' + new Date()
+        }
+    })  
+
+    var app3 = new Vue({
+        el: '#app-3',
+        data: {
+            seen: false
+        }
+    })
+</script>
