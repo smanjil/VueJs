143864820b839adecba8acb166ff57efe4f63f72
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-14 10:23:12 +0545

    vue-cli

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div id="app">
+
+        <message>Hello there!</message>
+        <message>Hello there again!</message>
+
+    </div>
+</template>
+
+<script>
+    import Message from './components/Message.vue';
+
+    export default {
+        name: 'app',
+
+        components: {
+            Message,
+        },
+
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+
+
+</style>
---
+++ hello-vue/src/components/Message.vue
@@ -0,0 +1,29 @@
+
+<template>
+
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+
+</template>
+
+<script>
+
+    export default{
+
+    }
+
+</script>
+
+<style>
+
+    .box{
+        background: #e3e3e3;
+        padding: 10px;
+        border: 1px solid #c5e5e5;
+        margin-bottom: 1em;
+    }
+
+</style>
